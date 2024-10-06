be4d3555632a9b940f8f9a91fc3631d27db2a42b
BohdanKoshkarov <bohdankharkov483@gmail.com>
2024-10-06 15:53:17 +0300

    Branche FEATURE commented function work

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function sayHi() {
     alert('Hello from Git!');
 }
 
-function work() {
-    alert('work work work work work');
-}
+// function work() {
+//    alert('work work work work work');
+//}
 
 function sayBye() {
     alert('Hello from Git!');
